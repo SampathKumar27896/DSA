b3298e5f93f49c511e1240a17f3b7efcf10e36fd
Sampath Kumar <sampathkumar27896@hotmail.com>
2024-01-13 00:25:57 +0530

    Worked on basic operations on linked list

--- js/app.js
+++ js/app.js
@@ -1,18 +1,7 @@
 const LinkedList = require('./linked-list.js');
-
-const list = new LinkedList();
-list.addNodesAtEnd([1, 2, 3, 4, 5]);
+//Program to delete the tail of the linked list
+const list = new LinkedList([1, 2, 34, 56, 66, 67]);
 list.printList();
-
-let temp = list.getHead();
-let prev = null;
-let front = null;
-while(temp !== null) {
-    front = temp.next;
-    temp.next = prev;
-    prev = temp;
-    temp = front;
-}
-
+const newHead = list.addNodeBeforeKthElement(1, 44, list.getHead());
+list.setHead(newHead);
 list.printList();
-
--- js/linked-list.js
+++ js/linked-list.js
@@ -8,6 +8,13 @@ class Node {
 
 class LinkedList {
     head;
+    constructor(nodes) {
+        if(Array.isArray(nodes)) {
+            this.addNodesAtEnd(nodes);
+        } else {
+            this.setHead(new Node(nodes));
+        }
+    }
     setHead(node) {
         this.head = node;
     }
@@ -21,6 +28,8 @@ class LinkedList {
         }
         return temp;
     }
+    // Program to convert a array to a linked list
+    // Program to add nodes at the end of the linked list
     addNodesAtEnd(data) {
         if(Array.isArray(data)) {
             if(this.getHead() === undefined) {
@@ -47,6 +56,7 @@ class LinkedList {
         printString += `NULL`;
         console.log(printString);
     }
+    // Program to find the length of the linked list
     length() {
         let temp = this.getHead();
         let count = 0;
@@ -62,6 +72,68 @@ class LinkedList {
             temp = temp.next;
         }
         return temp;
+    }
+    // Program to find whether the element exist in the linked list or not.
+    checkElementExist(elementToFind) {
+        let temp = this.getHead();
+        while(temp !== null) {
+            if(temp.data === elementToFind) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    // Program to delete the Kth element of the linked list
+    deleteKthElement(k, head) {
+        let count = 1;
+        let temp = head, current = null, next = null, nextAfterNext = null;
+        if(count === k) {
+            head = head.next;
+        } else {
+            while(temp !== null) {
+                count = count + 1;
+                current = temp;
+                next = temp.next;
+                nextAfterNext = next.next;
+                if(count === k) {
+                    current.next = nextAfterNext;
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    //Program to add node at the head of the linked list
+    addNodeAtHead(data, head) {
+        let newNode = new Node(data);
+        newNode.next = head;
+        return newNode;
+    }
+
+    //Program to add node before the Kth element of the linked list
+    addNodeBeforeKthElement(k, data, head) {
+        let newNode = new Node(data);
+        let temp = head, count = 0, currentNode = null, nextNode = null, nextAfterNextNode = null;
+        while(temp !== null) {
+            count = count + 1;
+            currentNode = temp;
+            nextNode = currentNode.next;
+            nextAfterNextNode = nextNode.next;
+            if(k === 1) {
+                return this.addNodeAtHead(data, head);
+            }
+            if(count === (k - 1)) {
+                currentNode.next = newNode;
+                newNode.next = nextNode;
+                nextNode.next = nextAfterNextNode;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
     }
 }
 
